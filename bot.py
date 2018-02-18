cc2151f866416623e8ce8db86475e1bc33a4b3e2
DESKTOP-NBN4UD9\spaeh <spaehanson1@gmail.com>
2018-02-17 22:07:59 -0600

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 17:02:39 2017
+
+@author: spaeh
+"""
+
+import discord
+import pymongo
+import json
+from discord.ext import commands
+import markov
+
+# establish a connection to the database
+connection = pymongo.MongoClient("mongodb://localhost")
+
+# get a handle to the school database
+db = connection.bot
+users = db.users
+
+
+bot = commands.Bot(command_prefix='??', description='Steve\'s robot')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------------')
+    
+    
+@bot.command()
+async def car():
+    await bot.say("beep beep!")
+
+@bot.command()
+async def whois(user: str):
+    try:
+        result = users.find_one({'User': user})
+    except Exception as e:
+        print("Unexpected error:", type(e), e)
+        
+    await bot.say('%s is %s' % (user, result['Name']))    
+
+@bot.command()
+async def whereis(user: str):
+    try:
+        result = users.find_one({'User': user})
+    except Exception as e:
+        print("Unexpected error:", type(e), e)
+    if(type(result) != None):
+        await bot.say('%s is %s' % (user, result['Address']))   
+    
+@bot.command()
+async def info(user: str):
+    try:
+        result = users.find_one({'User': user})
+    except Exception as e:
+        print("Unexpected error:", type(e), e)
+    if(type(result) != None):    
+        await bot.say('%s or \'%s\' lives at %s, phone: %s email: %s' % (result['User'],result['Name'],result['Address'],result['Phone'],result['email'])) 
+
+@bot.command()
+async def Kyle():
+    await bot.say(markov.markovChain('Kyle'))        
+    
+@bot.command()
+async def shrimpy():
+    await bot.say(markov.markovChain('shrimpy'))    
+
+@bot.command()
+async def Shrimpy():
+    await bot.say(markov.markovChain('shrimpy'))            
+    
+@bot.command()
+async def updateUser(user: str, field: str, value: str):
+
+    try:
+        users.update({'User': user}, {'$set': {field:value}})
+    except Exception as e:
+        print("Unexpected error:", type(e), e)
+            
+    await bot.say('Updated user %s\' field %s with new value %s' % (user,field,value))
+    
+@bot.command(pass_context = True)
+async def hello(ctx, member: discord.Member = None):  
+    if member is None:
+        member = ctx.message.author
+        print('switched to author')
+    print(str(ctx.message.author) + "called hello")
+    await bot.say(':slight_smile: fuck you ' + str(member))
+
+
+auth = json.load(open('auth.json'))
+bot.run(auth['token'])
---
+++ markov.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 19:50:03 2018
+
+@author: spaeh
+"""
+
+import random
+
+def buildChain(text, chain = {}):
+    words = text.split(' ')
+    index = 1
+    for word in words[index:]:
+        key = words[index - 1]
+        if key in chain:
+            chain[key].append(word)
+        else:
+            chain[key] = [word]
+        index += 1
+    return chain
+
+def generateMessage(chain, count = 100):
+    word1 = random.choice(list(chain.keys()))
+    message = word1.capitalize()
+
+    while len(message.split(' ')) < count:
+        word2 = random.choice(chain[word1])
+        word1 = word2
+        message += ' ' + word2
+    
+    return message
+
+#Opening local file
+def readFile(filename):
+    try:
+        with open(filename, "r") as file:
+            contents = file.read().replace('\n\n',' ')
+    except Exception as e:
+        print("Unexpected error:", type(e), e)
+    
+    return contents
+
+def writeFile(filename, message):
+    with open(filename, "w") as file:
+        file.write(message)
+                   
+def markovChain(user):
+    if user == 'Kyle':
+        message = readFile('snarf.txt')
+    if user == 'shrimpy':
+        message = readFile('shrimpy.txt')
+        message = message.split('\n')
+        message = message[random.randint(0,len(message)-1)]
+        return message
+    chain = buildChain(message)
+    message = generateMessage(chain)
+    message = message.split('\n')
+    message = message[random.randint(0,len(message)-1)]
+    return message    
+    
+print(markovChain('shrimpy'))
