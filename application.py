b00e7f4d53156dd83383e25881f52ab167f1dd6e
DESKTOP-NBN4UD9\spaeh <spaehanson1@gmail.com>
2020-04-10 19:55:05 -0500

    update say to send for discord changes

--- application.py
+++ application.py
@@ -32,8 +32,8 @@ async def on_ready():
     
     
 @bot.command()
-async def commands():
-    await bot.say("Commands: whois <user>, whereis <user>, info <user>, Kyle, "
+async def commands(ctx):
+    await ctx.send("Commands: whois <user>, whereis <user>, info <user>, Kyle, "
                   "shrimpy, Shrimpy, updateUser <user> <field> <attribute>,"
                   "roll <size> <number>, hello, deck, draw <number>, deckStatus")
 
@@ -43,84 +43,84 @@ async def hello(ctx, member: discord.Member = None):
         member = ctx.message.author
         print('switched to author')
     print(str(ctx.message.author) + "called hello")
-    await bot.say(':slight_smile: hello ' + str(member))
+    await ctx.send(':slight_smile: hello ' + str(member))
 
 @bot.command()
-async def whois(user: str):
+async def whois(ctx, user: str):
     try:
         result = users.find_one({'User': re.compile(user, re.IGNORECASE)})
     except Exception as e:
         print("Unexpected error:", type(e), e)
         
-    await bot.say('%s is %s' % (user, result['Name']))    
+    await ctx.send('%s is %s' % (user, result['Name']))    
 
 @bot.command()
-async def whereis(user: str):
+async def whereis(ctx, user: str):
     try:
         result = users.find_one({'User': re.compile(user, re.IGNORECASE)})
     except Exception as e:
         print("Unexpected error:", type(e), e)
     if(type(result) != None):
-        await bot.say('%s is %s' % (user, result['Address']))   
+        await ctx.send('%s is %s' % (user, result['Address']))   
     
 @bot.command()
-async def info(user: str):    
+async def info(ctx, user: str):    
     try:
         result = users.find_one({'User': re.compile(user, re.IGNORECASE)})
     except Exception as e:
         print("Unexpected error:", type(e), e)
     if(type(result) != None):    
-        await bot.say('%s or \'%s\' lives at %s, phone: %s email: %s' % (result['User'],result['Name'],result['Address'],result['Phone'],result['email'])) 
+        await ctx.send('%s or \'%s\' lives at %s, phone: %s email: %s' % (result['User'],result['Name'],result['Address'],result['Phone'],result['email'])) 
 
 @bot.command()
-async def updateUser(user: str, field: str, value: str):
+async def updateUser(ctx, user: str, field: str, value: str):
     try:
         users.update({'User': re.compile(user, re.IGNORECASE)}, {'$set': {field:value}})
     except Exception as e:
         print("Unexpected error:", type(e), e)
             
-    await bot.say('Updated user %s\' field %s with new value %s' % (user,field,value))
+    await ctx.send('Updated user %s\' field %s with new value %s' % (user,field,value))
 
 @bot.command()
-async def Kyle():
-    await bot.say(markov.markovChain('Kyle'))        
+async def Kyle(ctx):
+    await ctx.send(markov.markovChain('Kyle'))        
     
 @bot.command()
-async def shrimpy():
-    await bot.say(markov.markovChain('shrimpy'))    
+async def shrimpy(ctx):
+    await ctx.send(markov.markovChain('shrimpy'))    
 
 @bot.command()
-async def Shrimpy():
-    await bot.say(markov.markovChain('shrimpy'))            
+async def Shrimpy(ctx):
+    await ctx.send(markov.markovChain('shrimpy'))            
 
 @bot.command()
-async def roll(dice = 6,number = 1):
+async def roll(ctx, dice = 6, num_of_dice = 1):
     results = ''
     
-    for die in range(number):
+    for die in range(num_of_dice):
         results += str(random.randint(1,dice)) + ', '
     results = results[:-2]
     
-    await bot.say('You rolled %s' % results)
+    await ctx.send('You rolled %s' % results)
 
 @bot.command()
-async def deck():
+async def deck(ctx):
     cards.newGame()
-    await bot.say('Created a new deck of cards')
+    await ctx.send('Created a new deck of cards')
     
 @bot.command(pass_context = True)
 async def deal(ctx,number = 1):
     member = ctx.message.author
-    await bot.say(cards.deal(str(member), number))
+    await ctx.send(cards.deal(str(member), number))
 
 @bot.command(pass_context = True)
 async def hand(ctx,number = 1):
     member = ctx.message.author
-    await bot.say(cards.hand(str(member)))   
+    await ctx.send(cards.hand(str(member)))   
     
 @bot.command()
-async def deckStatus():
-    await bot.say(cards.count())
+async def deckStatus(ctx):
+    await ctx.send(cards.count())
    
 
 auth = json.load(open('auth.json'))
